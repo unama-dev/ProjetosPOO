1687fec37f5873ec465845049f7ea889d2e4e482
adoniaspp <adoniaspp@gmail.com>
2019-03-13 20:47:27 -0300

    Initial commit

---
+++ src/ClassesObjetos/Crianca.java
@@ -0,0 +1,26 @@
+package ClassesObjetos;
+
+class Crianca {
+
+    String corCabelo;
+    String corPele;
+    double altura;
+    int idade;
+
+    void brincar(){
+        System.out.println("Brincando...");
+    }
+
+    void correr(double metros){
+        System.out.println("Correndo " + metros + " metros");
+    }
+
+    void nadar(double metros){
+        System.out.println("Nadando " + metros + " metros");
+    }
+
+    String sorrir() {
+        return ":)";
+    }
+
+}
